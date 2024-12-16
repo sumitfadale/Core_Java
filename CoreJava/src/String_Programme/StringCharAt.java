33a5d65ba306dc2345e7cfa6f3d3e41e713a750a
sumitfadale <fadalesumit.s@gmail.com>
2024-12-16 13:40:14 +0530

    add git ignore

--- CoreJava/src/String_Programme/StringCharAt.java
+++ CoreJava/src/String_Programme/StringCharAt.java
@@ -5,7 +5,7 @@ public class StringCharAt {
 	public static void main(String[] args) {
      String str = "Hello";
      System.out.println("Charachter at : " + str.charAt(1));
-     //charAt method will return specified index.....
+     //charAt method will return specified index..
 	}
 
 }
